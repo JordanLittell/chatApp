046396fa4f58386003720f93b7240b6eae7ab32c
Jlit1001 <jordanlittell@yahoo.com>
2014-10-25 15:15:34 -0700

    added chat rooms and joining

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,13 +1,18 @@
+var actions = {
+  '/name': "nameChange",
+  '/room': "roomChange"
+}
+
 var getInput = function () {
   var message = $('.message').val(); 
   return message;
 }
 
 var submitMessage = function (chat) {
-  var msg = getInput()
+  var msg = getInput();
   var arr = msg.split(' ');
-  if (arr[0]==='/nick') {
-    chat.socket.emit('nameChange', {name:arr[1]});
+  if (msg[0]==='/') {
+    chat.socket.emit(actions[arr[0]], {name:arr[1]});
   } else {
     chat.sendMessage(msg);
   }
@@ -16,11 +21,17 @@ var submitMessage = function (chat) {
 $(document).ready(function() {
   var socket = io(); 
   var chat = chat || new window.Chat(socket);
+  
   chat.socket.on('nameChangeResponse', function (data) {
-    console.log(data.name);
-  })
+    $('.display').append( "<p> name changed to: " + data.name +'</p>');   
+  });
+  
   chat.socket.on('msgSent',function(data){
     $('.display').append( "<p>" + data.name + ": " + data.text + '</p>');   
+  });
+
+  chat.socket.on('roomChangeRes', function(data){
+    $('.display').append("<p>You have joined room: " + data.room + "</p>");
   })
 
   $('form').on('submit', function(event) {
